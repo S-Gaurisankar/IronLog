5a093398c432669de5d8162660f6f23175e41775
S Gaurisankar <gaurisankar2003@gmail.com>
2026-06-14 22:22:07 +0530

    Removed hub

--- src/constants/navbar.constants.ts
+++ src/constants/navbar.constants.ts
@@ -1,9 +1,7 @@
 export const NAVBAR_CONSTANTS = {
-    HUB: 'Hub',
     LOG: 'Logs',
     CREATE: 'Track',
     PROFILE: 'Profile',
-    HUB_HREF: '/hub',
     LOG_HREF: '/logs',
     TRACK_HREF: '/track',
     PROFILE_HREF: '/profile',
